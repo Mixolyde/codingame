ce5a08693c8540625dab127951932b5df11803d0
Brian Grey <mixolyde@gmail.com>
2022-01-18 11:54:18 -0500

    Update tests

--- test/codersstrikeback_test.dart
+++ test/codersstrikeback_test.dart
@@ -1,28 +1,43 @@
 import '../bin/codersstrikeback.dart';
 import 'package:test/test.dart';
 
-void main() {
+void main() {  
+  group('dart math', (){
+    test('truncate', (){
+      expect((2.1).truncate(), 2);
+      expect((2.9).truncate(), 2);
+      expect((-2.1).truncate(), -2);
+      expect((-2.9).truncate(), -2);      
+    });
+  });
   group('pods:', () {
     test('new pod', () {
       Pod a = new Pod(0, 1, 2, 3, 4, 5, 6);
-      expect(a.id, equals(0));
-      expect(a.x, equals(1));
-      expect(a.y, equals(2));
-      expect(a.vx, equals(3));
-      expect(a.vy, equals(4));
-      expect(a.angle, equals(5));
-      expect(a.nextCP, equals(6));
-      expect(a.radius, equals(400));
+      expect(a.id, 0);
+      expect(a.x, 1);
+      expect(a.y, 2);
+      expect(a.vx, 3);
+      expect(a.vy, 4);
+      expect(a.angle, 5);
+      expect(a.nextCP, 6);
+      expect(a.radius, 400);
+      expect(a.timeout, 100);
+      expect(a.shield, false);
+      expect(a.shieldTimeout, 0);
+      expect(a.checked, 0);
+      expect(a.boostUsed, false);
+      
     });
     test('clone pods', () {
       Pod a = new Pod(0, 0, 0, 0, 0, 0, 0);
       a.timeout = 50;
       a.shield = false;
       Pod b = a.clone();
-      expect(a.timeout == b.timeout, equals(true));
-      expect(a.shield == b.shield, equals(true));
-      expect(a.id == b.id, equals(true));
+      expect(a.timeout, b.timeout);
+      expect(a.shield, b.shield);
+      expect(a.id , b.id);
       a.timeout = 0;
+      expect(a.timeout, equals(00));
       expect(b.timeout, equals(50));
     });
     test('get angles', () {
@@ -61,6 +76,8 @@ void main() {
       expect(a.id, 1);
       expect(a.x, 4);
       expect(a.y, 5);
+      expect(a.vx, 0);
+      expect(a.vy, 0);      
       expect(a.radius, 200);
     });
   });
